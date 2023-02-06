ce385ec12ffea804a02b0305d929ba61aa25e4cd
Your Name <pavel.novoa@gmail.com>
2023-02-07 00:47:26 +0100

    some impvs

--- evaluation.R
+++ evaluation.R
@@ -1,5 +1,6 @@
 rm(list = ls())
 library(tidyverse)
+library(ggbump)
 
 # Reading the case study data
 
@@ -108,16 +109,16 @@ df_stats <- tibble(
 
 # Plots
 
-library(ggbump)
-step = 0.5
+
+step = 0.2
 df_plot_ <- lapply(split(df_final, df_final$NF.Rank), function(grp_df){
-  grp_df$NF.Rank = grp_df$NF.Rank + step*c(0:(nrow(grp_df)-1))
+  grp_df$NF.Rank = grp_df$NF.Rank #+ step*c(0:(nrow(grp_df)-1))
   grp_df
 })
 
 df_plot_ <- bind_rows(df_plot_)
 
-df_plot <- df_plot_ %>% filter(NF.Rank < 10) %>%
+df_plot <- df_plot_ %>% filter(NF.Rank < 60) %>%
   select(Estudiante, ends_with(".Rank")) %>%
   rename(
     "Tradicional" = NF.Rank,
@@ -131,26 +132,63 @@ df_plot <- df_plot_ %>% filter(NF.Rank < 10) %>%
   ) %>%
   mutate(
     `Método` = factor(`Método`, levels=c("Tradicional", "ROC", "RS", "RR")),
-    Estudiante = factor(Estudiante)
+    Estudiante = factor(Estudiante),
+    PosRank = floor(`Posición`)
   )
 
 
 
 
-df_plot %>%
+p_rank_comp <- df_plot %>%
   ggplot(aes(x = as.numeric(`Método`), y = `Posición`, color = Estudiante)) +
-  geom_bump(size = 1) +
-  geom_point(size = 4) +
-  geom_text(data = df_plot %>% filter(`Método` == "Tradicional"),
-            aes(x = as.numeric(`Método`) - 0.1, label = Estudiante),
-            size = 2.5, hjust = 1) +
-  geom_text(data = df_plot %>% filter(`Método` == "RR"),
-            aes(x = as.numeric(`Método`) + 0.1, label = Estudiante),
-            size = 2.5, hjust = 0) +
-  scale_x_continuous(breaks = 1:4, labels = c("Tradicional", "ROC", "RS", "RR")) +
+  geom_bump(linewidth = 1, alpha=0.8) +
+  geom_point(aes(fill = Estudiante), size = 7, shape=19, alpha=0.8) + # fill="white", stroke=2) +
+  #geom_text(data = df_plot %>% filter(`Método` == "Tradicional"),
+  #          aes(x = as.numeric(`Método`) - 0.1, label = Estudiante),
+  #            size = 2.5, hjust = 1) +
+  #geom_text(data = df_plot %>% filter(`Método` == "RR"),
+  #            aes(x = as.numeric(`Método`) + 0.1, label = Estudiante),
+  #          size = 2.5, hjust = 0) +
+  geom_text(aes(label = PosRank), color="black",
+            size = 3) +
+  scale_x_continuous(breaks = 1:4,
+                     labels = c("Enfoque\nTradicional", "Método\nROC", "Método\nRS", "Método\nRR"),
+                     limits = c(0.8,4.2),
+                     position = "top") +
   xlab("") +
+  ylab("") +
   scale_y_reverse() +
+  theme_void() +
+  theme(legend.position = "none",
+        axis.text.y=element_blank(),
+        axis.text.x = element_text(face = "bold")
+        )
+
+print(p_rank_comp)
+
+ggsave("p_rank_comp.pdf", plot = p_rank_comp, width = 8, height = 11)
+
+
+df_weights <- df_preferences %>%
+  mutate(
+    `Método ROC` = roc_ws,
+    `Método RS` = rs_ws,
+    `Método RR` = rr_ws
+  ) %>%
+  select(`Evaluación parcial`, Pesos, `Método ROC`, `Método RS`, `Método RR`) %>%
+  rename(
+    "Enf. tradicional" = Pesos
+  ) %>%
+  pivot_longer(cols = -c(`Evaluación parcial`), names_to = "Método", values_to = "Peso") %>%
+  mutate(`Método` = factor(`Método`, levels=c("Enf. tradicional", "Método ROC", "Método RS", "Método RR")))
+
+p_weight_comp <- df_weights %>%
+  ggplot(aes(x=`Evaluación parcial`, y=Peso, group=`Método`, color=`Método`))+
+  geom_line() +
+  geom_point(size=2) +
   theme_minimal() +
-  theme(legend.position = "none")
+  ggtitle("Distribución de los pesos") +
+  theme(legend.position = c(0., 0.25))
 
 
+ggsave("p_weights.pdf", plot = p_weight_comp, width = 4, height = 3)
